987954a501c550f63e18efe4c4f08c7da44dfb61
sergei <sergei.gots@yahoo.com>
2025-02-19 02:08:48 +0300

    L65. Release Db resources: resultSet, statement -n- connection

--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -42,6 +42,21 @@ public class AuthorDaoImpl implements AuthorDao{
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         return null;
