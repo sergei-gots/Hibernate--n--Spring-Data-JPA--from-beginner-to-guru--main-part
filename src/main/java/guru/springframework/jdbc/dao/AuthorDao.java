90a85a642422d731101444a10d42a189a415523d
sergei <sergei.gots@yahoo.com>
2025-02-20 01:16:43 +0300

    Seciton 10. Assignment 8. Prepare Author DAO Impl stub.

--- src/main/java/guru/springframework/jdbc/dao/AuthorDao.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDao.java
@@ -8,10 +8,13 @@ import guru.springframework.jdbc.domain.Author;
 public interface AuthorDao {
 
     Author getById(Long id);
+
     Author findAuthorByName(String firstName, String lastName);
+
     Author saveNewAuthor(Author author);
 
     Author updateAuthor(Author author);
 
     void deleteAuthorById(Long id);
+
 }
--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -3,210 +3,33 @@ package guru.springframework.jdbc.dao;
 import guru.springframework.jdbc.domain.Author;
 import org.springframework.stereotype.Component;
 
-import javax.sql.DataSource;
-import java.sql.*;
-
 /**
- * Created by sergei on 18/02/2025
+ *   Created by sergei on 20/02/2025
  */
 @Component
-public class AuthorDaoImpl implements AuthorDao{
-
-    private final DataSource dataSource;
-
-    public AuthorDaoImpl(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
+public class AuthorDaoImpl implements AuthorDao {
     @Override
     public Author getById(Long id) {
-
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
-
-        try {
-            connection = dataSource.getConnection();
-            ps = connection.prepareStatement("SELECT * FROM author WHERE id = ?");
-            ps.setLong(1, id);
-            resultSet = ps.executeQuery();
-
-            if (resultSet.next()) {
-                return getAuthorFromRs(resultSet);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                closeAll(resultSet, ps, connection);
-
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
-        }
-
         return null;
     }
 
     @Override
     public Author findAuthorByName(String firstName, String lastName) {
-
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
-
-        try {
-            connection = dataSource.getConnection();
-            ps = connection.prepareStatement("SELECT * FROM author WHERE first_name = ? AND last_name = ?");
-            ps.setString(1, firstName);
-            ps.setString(2, lastName);
-            resultSet = ps.executeQuery();
-
-            if (resultSet.next()) {
-                return getAuthorFromRs(resultSet);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        finally {
-            try {
-                closeAll(resultSet, ps, connection);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
         return null;
     }
 
     @Override
     public Author saveNewAuthor(Author author) {
-
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
-
-        try {
-            connection = dataSource.getConnection();
-
-            ps = connection.prepareStatement("INSERT INTO author(first_name, last_name, country) VALUES(?, ?, ?)");
-            ps.setString(1, author.getFirstName());
-            ps.setString(2, author.getLastName());
-            ps.setString(3, author.getCountry());
-            ps.execute();
-
-            Statement statement = connection.createStatement();
-            //Note: the next query "SELECT LAST_INSER_ID() is MySQL specific
-            resultSet = statement.executeQuery("SELECT LAST_INSERT_ID()");
-            if (resultSet.next()) {
-                Long savedId = resultSet.getLong(1);
-                return this.getById(savedId);
-            }
-            statement.close();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        finally {
-
-            try {
-                closeAll(resultSet, ps, connection);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
-        }
-
         return null;
     }
 
     @Override
     public Author updateAuthor(Author author) {
-
-        Connection connection = null;
-        PreparedStatement ps = null;
-
-        try {
-            connection = dataSource.getConnection();
-
-            ps = connection.prepareStatement("UPDATE author SET first_name = ?, last_name = ?, country = ? WHERE id = ?");
-            ps.setString(1, author.getFirstName());
-            ps.setString(2, author.getLastName());
-            ps.setString(3, author.getCountry());
-            ps.setLong(4, author.getId());
-            ps.execute();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        finally {
-
-            try {
-                closeAll(null, ps, connection);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
-        }
-
-        return this.getById(author.getId());
+        return null;
     }
 
     @Override
     public void deleteAuthorById(Long id) {
 
-        Connection connection = null;
-        PreparedStatement ps = null;
-
-        try {
-            connection = dataSource.getConnection();
-
-            ps = connection.prepareStatement("DELETE FROM author WHERE id = ?");
-            ps.setLong(1, id);
-            ps.execute();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        finally {
-
-            try {
-                closeAll(null, ps, connection);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
-        }
-
-    }
-
-    private static Author getAuthorFromRs(ResultSet resultSet) throws SQLException {
-
-        Author author = new Author();
-
-        author.setId(resultSet.getLong("id"));
-        author.setFirstName(resultSet.getString("first_name"));
-        author.setLastName(resultSet.getString("last_name"));
-        author.setCountry(resultSet.getString("country"));
-
-        return author;
-
-    }
-
-    private static void closeAll(ResultSet resultSet, PreparedStatement ps, Connection connection) throws SQLException {
-
-        if (resultSet != null) {
-            resultSet.close();
-        }
-
-        if (ps != null) {
-            ps.close();
-        }
-
-        if (connection != null) {
-            connection.close();
-        }
     }
 }
