943b4aab7accc7546bce298ae247e31246762bbc
sergei <sergei.gots@yahoo.com>
2025-02-21 02:28:03 +0300

    Lesson 85. Hibernate DAO: starting state

--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -1,83 +1,36 @@
 package guru.springframework.jdbc.dao;
 
 import guru.springframework.jdbc.domain.Author;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Component;
 
 /**
- *   Created by sergei on 20/02/2025
+ * Created by sergei on 21/02/2025
  */
 @Component
 public class AuthorDaoImpl implements AuthorDao {
 
-    private final static String SQL_GET_AUTHOR_LEFT_OUTER_JOIN_BOOK_BASE =
-        """
-            SELECT author.*, book.id as book_id, book.title, book.isbn, book.publisher
-            FROM author
-            LEFT OUTER JOIN book ON book.author_id = author.id
-        """;
-
-    private final JdbcTemplate jdbcTemplate;
-
-    public AuthorDaoImpl(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
     @Override
     public Author getById(Long id) {
-
-
-        return jdbcTemplate.query(
-                SQL_GET_AUTHOR_LEFT_OUTER_JOIN_BOOK_BASE + " WHERE author.id = ?",
-                getAuthorResultSetExtractor(),
-                id);
+        return null;
     }
-
 
     @Override
     public Author findAuthorByName(String firstName, String lastName) {
-
-        return jdbcTemplate.query(
-                SQL_GET_AUTHOR_LEFT_OUTER_JOIN_BOOK_BASE + " WHERE first_name = ? AND last_name = ?",
-                getAuthorResultSetExtractor(),
-                firstName, lastName
-        );
+        return null;
     }
 
     @Override
     public Author saveNewAuthor(Author author) {
-
-        jdbcTemplate.update(
-                "INSERT INTO author(first_name, last_name, country) VALUES(?, ?, ?)",
-                author.getFirstName(), author.getLastName(), author.getCountry()
-        );
-
-        Long savedId = jdbcTemplate.queryForObject("SELECT LAST_INSERT_ID()", Long.class);
-
-        return this.getById(savedId);
+        return null;
     }
 
     @Override
     public Author updateAuthor(Author author) {
-
-        jdbcTemplate.update("UPDATE author SET first_name = ?, last_name = ?, country = ? WHERE id = ?",
-                author.getFirstName(), author.getLastName(), author.getCountry(), author.getId()
-        );
-
-        return this.getById(author.getId());
+        return null;
     }
 
     @Override
     public void deleteAuthorById(Long id) {
 
-        jdbcTemplate.update("DELETE FROM author WHERE id = ?", id);
-    }
-
-    /** This method is applied to get a new instance of ResultSetExtractor<Author> in order
-     * to provide thread safety
-     */
-    private ResultSetExtractor<Author> getAuthorResultSetExtractor() {
-        return new AuthorResultSetExtractor();
     }
 }
--- src/main/java/guru/springframework/jdbc/dao/BookDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/BookDaoImpl.java
@@ -1,73 +1,35 @@
 package guru.springframework.jdbc.dao;
 
-import guru.springframework.jdbc.domain.Author;
 import guru.springframework.jdbc.domain.Book;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 /**
- *   Created by sergei on 20/02/2025
+ * Created by sergei on 21/02/2025
  */
 @Component
 public class BookDaoImpl implements BookDao {
-
-    private final JdbcTemplate jdbcTemplate;
-
-    public BookDaoImpl(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
     @Override
     public Book getById(Long id) {
-
-        return jdbcTemplate.queryForObject("SELECT * FROM book WHERE id  = ?", getBookRowMapper(), id);
+        return null;
     }
 
     @Override
     public Book findBookByTitle(String title) {
-
-        return jdbcTemplate.queryForObject("SELECT * FROM book WHERE title  = ?", getBookRowMapper(), title);
+        return null;
     }
 
     @Override
-    public Book saveNewBook(Book book) {
-
-        Author author = book.getAuthor();
-
-        jdbcTemplate.update("INSERT INTO book (isbn, publisher, title, author_id) VALUES(?, ?, ?, ?)",
-         book.getIsbn(), book.getPublisher(), book.getTitle(),
-                author != null ? author.getId() : null
-        );
-
-        Long savedId = jdbcTemplate.queryForObject("SELECT LAST_INSERT_ID()", Long.class);
-
-        return this.getById(savedId);
+    public Book saveNewBook(Book Book) {
+        return null;
     }
 
     @Override
-    public Book updateBook(Book book) {
-
-        Author author = book.getAuthor();
-
-        jdbcTemplate.update("UPDATE book SET isbn = ?, publisher = ?, title = ?, author_id = ? WHERE id = ?",
-                book.getIsbn(), book.getPublisher(), book.getTitle(),
-                author != null ? author.getId() : null,
-                book.getId()
-        );
-
-        return this.getById(book.getId());
+    public Book updateBook(Book Book) {
+        return null;
     }
 
     @Override
     public void deleteBookById(Long id) {
 
-        jdbcTemplate.update("DELETE FROM book WHERE id = ?", id);
-
     }
-
-    private RowMapper<Book> getBookRowMapper() {
-        return new BookRowMapper();
-    }
-
 }
