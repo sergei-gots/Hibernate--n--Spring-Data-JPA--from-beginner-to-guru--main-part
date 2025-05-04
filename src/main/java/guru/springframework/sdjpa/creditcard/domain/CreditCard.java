71871316498217be82c3d44c4e6a7dae9dc192e4
sergei <sergei.gots@yahoo.com>
2025-05-04 19:30:15 +0300

    Add Lombok

--- src/main/java/guru/springframework/sdjpa/creditcard/domain/CreditCard.java
+++ src/main/java/guru/springframework/sdjpa/creditcard/domain/CreditCard.java
@@ -6,13 +6,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
  * Created by sergei on 26/04/2025
  */
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class CreditCard {
 
     @Id
@@ -26,58 +34,5 @@ public class CreditCard {
     private String cvv;
 
     private String expirationDate;
-
-    public CreditCard() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getCreditCardNumber() {
-        return creditCardNumber;
-    }
-
-    public void setCreditCardNumber(String creditCardNumber) {
-        this.creditCardNumber = creditCardNumber;
-    }
-
-    public String getCvv() {
-        return cvv;
-    }
-
-    public void setCvv(String cvv) {
-        this.cvv = cvv;
-    }
-
-    public String getExpirationDate() {
-        return expirationDate;
-    }
-
-    public void setExpirationDate(String expirationDate) {
-        this.expirationDate = expirationDate;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        CreditCard that = (CreditCard) o;
-        return Objects.equals(id, that.id) && Objects.equals(creditCardNumber, that.creditCardNumber) && Objects.equals(cvv, that.cvv) && Objects.equals(expirationDate, that.expirationDate);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, creditCardNumber, cvv, expirationDate);
-    }
-
-    @Override
-    public String toString() {
-        return "CreditCard{" +
-                "id=" + id +
-                ", creditCardNumber='" + creditCardNumber + '\'' +
-                ", cvv='" + cvv + '\'' +
-                ", expirationDate='" + expirationDate + '\'' +
-                '}';
-    }
 
 }
