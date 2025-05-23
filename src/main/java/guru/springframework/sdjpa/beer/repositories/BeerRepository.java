1aac46861d65a22185b5fb4a8803c6b8c62d8265
sergei <sergei.gots@yahoo.com>
2025-05-24 00:20:37 +0300

    Lesson 246. K8s example. Pom: spring-boot-starter-data-rest dependency removed

--- src/main/java/guru/springframework/sdjpa/beer/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/sdjpa/beer/repositories/BeerRepository.java
@@ -5,14 +5,12 @@ import guru.springframework.sdjpa.beer.enums.BeerStyle;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.UUID;
 
 /**
  * Created by sergei on 11/05/2025
  */
-@RepositoryRestResource(path = "beer", collectionResourceRel = "beer")
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
 
     Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
